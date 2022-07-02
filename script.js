dfadd494b55bf413e47223f889dbc651aaf6a4c3
Ashu <86954013+gitx-ashu@users.noreply.github.com>
2022-07-02 16:35:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert ("Use PC or Laptop for Better Experience\n\nHope you will like the Code!");
+alert ("Hope you will like the Code!\nUse PC or Laptop for Better Experience");
 var a = new Audio("https://dl.dropbox.com/s/e93f5kxsmtankf3/Best%20Happy%20Birthday%20To%20You%20_%20Happy%20Birthday%20Songs%20Remix%202020%28M4A_128K%29_1.m4a?dl=0");
 
 window.addEventListener('click',() => {
